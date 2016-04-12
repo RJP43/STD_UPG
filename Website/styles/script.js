30c0fa5b8f6890e1a1ad120a6587b1869a31adcd
Rebecca Parker <rjp43@pitt.edu>
2016-04-12 17:50:37 -0400

    edits to site and adding images

--- Website/styles/script.js
+++ Website/styles/script.js
@@ -6,32 +6,39 @@ function rollover(my_image) {
 function mouseaway(my_image) {
     my_image.src = "images/SigmaTauDeltaEmblem.jpg";
 }
+
 function init() {
     var buttons = document.getElementsByTagName("button")
     
     var button1 = buttons[0]
     button1.addEventListener('click', show_hide, false);
-/*    var button2 = buttons[1]
-    button2.addEventListener('click', show_hide1, false);
-    var button3 = buttons[2]
-    button3.addEventListener('click', show_hide2, false);*/
-
-function show_hide() {
-    if (document.getElementById('toggle').style.display != "none") {
-        document.getElementById('toggle').style.display = "none";
-    } else {
-        document.getElementById('toggle').style.display = "block";
+    
+    
+    function show_hide() {
+        if (document.getElementById('toggle').style.display != "none") {
+            document.getElementById('toggle').style.display = "none";
+        } else {
+            document.getElementById('toggle').style.display = "block";
+        }
     }
-/*function show_hide1() {
-    if (document.getElementById('toggle1').style.display != "none") {
-        document.getElementById('toggle1').style.display = "none";
-    } else {
-        document.getElementById('toggle1').style.display = "block";
+    
+    var button2 = buttons[0]
+    button2.addEventListener('click', show_hide1, false);
+    var button3 = buttons[1]
+    button3.addEventListener('click', show_hide2, false);
+    
+    function show_hide1() {
+        if (document.getElementById('toggle1').style.display != "none") {
+            document.getElementById('toggle1').style.display = "none";
+        } else {
+            document.getElementById('toggle1').style.display = "block";
+        }
     }
     function show_hide2() {
-    if (document.getElementById('toggle2').style.display != "none") {
-        document.getElementById('toggle2').style.display = "none";
-    } else {
-        document.getElementById('toggle2').style.display = "block";
-    }}}*/
-    }}
+        if (document.getElementById('toggle2').style.display != "none") {
+            document.getElementById('toggle2').style.display = "none";
+        } else {
+            document.getElementById('toggle2').style.display = "block";
+        }
+    }
+}
