e147fb3d93c3812af87bd8907efab8d4543dd9de
Rebecca Parker <rjp43@pitt.edu>
2016-04-11 22:58:13 -0400

    edits to new site and adding photos

--- Website/styles/script.js
+++ Website/styles/script.js
@@ -11,11 +11,27 @@ function init() {
     
     var button1 = buttons[0]
     button1.addEventListener('click', show_hide, false);
-}
+/*    var button2 = buttons[1]
+    button2.addEventListener('click', show_hide1, false);
+    var button3 = buttons[2]
+    button3.addEventListener('click', show_hide2, false);*/
+
 function show_hide() {
     if (document.getElementById('toggle').style.display != "none") {
         document.getElementById('toggle').style.display = "none";
     } else {
-        document.getElementById("toggle").style.display = "block";
+        document.getElementById('toggle').style.display = "block";
     }
-}
+/*function show_hide1() {
+    if (document.getElementById('toggle1').style.display != "none") {
+        document.getElementById('toggle1').style.display = "none";
+    } else {
+        document.getElementById('toggle1').style.display = "block";
+    }
+    function show_hide2() {
+    if (document.getElementById('toggle2').style.display != "none") {
+        document.getElementById('toggle2').style.display = "none";
+    } else {
+        document.getElementById('toggle2').style.display = "block";
+    }}}*/
+    }}
